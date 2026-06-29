b8c58c4a921ae3c4ff99f029140142b10f896075
lenashvec83-del <lena.shvec83@gmail.com>
2026-06-29 18:47:34 +0200

    cfg228

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleAnswer(id) {
+  const answer = document.getElementById(id);
+
+  if (answer.style.display === 'none' || !answer.style.display) {
+    answer.style.display = 'block';
+  } else {
+    answer.style.display = 'none';
+  }
+} 
